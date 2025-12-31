9df928fe2a25311e12387d4cc265ec4315dcc074
Oops19 <Oops19@users.noreply.github.com>
2025-12-31 02:51:00 +0100

    update

--- offline/patch_offline.py
+++ offline/patch_offline.py
@@ -16,14 +16,34 @@ class PatchOffline:
         #           <L n="specified">
 
         actions = {
-            'change_age': {
-                'xpath': "/I/L[@n='tests']/L/V[@t='sim_info']/../../../L[@n='tests']",
-                'delete': [{'tag': 'L'}, ],
+            'allow_for_everyone': {
+                'xpath': "/I/L[@n='tests']/L",
+                'delete': [{'tag': 'V', 'attrib': {'t': 'test_set_reference'}}, ],
+            },
+            'deny_for_everyone': {
+                'xpath': ".",
                 'add': [
-                    {'_xml': '<L><V t="sim_info"><U n="sim_info"><V n="ages" t="specified"><L n="specified"><E>TEEN</E><E>YOUNGADULT</E><E>ADULT</E><E>ELDER</E></L></V></U></V><V t="sim_info"><U n="sim_info"><V n="ages" t="specified"><L n="specified"><E>TEEN</E><E>YOUNGADULT</E><E>ADULT</E><E>ELDER</E></L></V><E n="who">OtherSimsInCurrentGame</E></U></V></L>'},
+                    {'tag': 'T', 'attrib': {'n': 'allow_user_directed'}, 'text': 'False'},
+                    {'tag': 'T', 'attrib': {'n': 'allow_autonomous'}, 'text': 'False'},
                 ],
             },
         }
+        actions = {
+            'filter': {
+                'xpath': "/I/L[@n='test']",
+                'delete': [{'tag': 'L', }, ],
+                'add': [
+                    {'_xml': '<L><V t="trait"><U n="trait"><L n="whitelist_traits"><T>274985</T></L></U></V></L>'}
+                ],
+
+                # 'add': [
+                #                     {'_xml': '<L><V t="sim_info"><U n="sim_info"><V n="ages" t="specified"><L n="specified"><E>TEEN</E><E>YOUNGADULT</E><E>ADULT</E><E>ELDER</E></L></V></U></V><V t="sim_info"><U n="sim_info"><V n="ages" t="specified"><L n="specified"><E>TEEN</E><E>YOUNGADULT</E><E>ADULT</E><E>ELDER</E></L></V><E n="who">OtherSimsInCurrentGame</E></U></V></L>'},
+                #                 ],
+            },
+        }
+
+        # <T n="allow_user_directed">False</T>
+        #   <T n="allow_autonomous">True</T>
 
         # {'tag': 'T', 'attrib': {'n': 'maximum_auto_satisfy_time'}, 'text': '0', },
         # {'tag': 'L', 'sim_age_test': ({'CHILD', 'TEEN', 'YOUNGADULT', 'ADULT', 'ELDER'}, None)}
